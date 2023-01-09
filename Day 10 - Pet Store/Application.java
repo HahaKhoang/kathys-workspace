8160436696157cdc98bf40d546015e5b8805e3aa
HahaKhoang <kathyhahoang@gmail.com>
2023-01-09 16:51:42 +0000

    Day 10 - Pet Store

---
+++ Day 10 - Pet Store/Application.java
@@ -0,0 +1,35 @@
+import java.util.List;
+import java.util.Scanner;
+
+/** 
+ * Application should be a repeating menu capable of adding, displaying, and removing pets
+ */
+
+public class Application {
+    public static void main(String[] args){
+        PetStoreService pss = new PetStoreService();
+        Scanner sc = new Scanner(System.in);
+        boolean shouldLoop = true;
+        while(shouldLoop){
+            System.out.println("Please choose: Add | Purchase | Exit");
+            List<Pet> allPets = pss.getPets();
+            System.out.println("Current pets: " + allPets);
+            String input = sc.nextLine();
+            if(input.equals("Add")){
+                // Fill in add behavior
+                String name = sc.nextLine();
+                String sound = sc.nextLine();
+                Pet newPet = new Pet(name, sound);
+                pss.addPet(newPet);
+
+            } else if(input.equals("Purchase")){
+                // Fill in purchase behavior
+                String name = sc.nextLine();
+                pss.purchasePet(name);
+            } else if(input.equals("Exit")){
+                shouldLoop = false;
+                // Will break the while loop next time the while loop condition is checked
+            }
+        }
+    }
+}
